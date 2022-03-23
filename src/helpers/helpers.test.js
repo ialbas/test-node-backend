7cc4c798f1f4a3b21583f10fad8a3db572033823
Ialbas Junior <ialbasjunior@gmail.com>
2022-03-23 10:16:00 -0300

    resolve test encripter and token

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -5,8 +5,9 @@ const MissingParamError = require('./missing-param-error')
 describe('Ensure works of Encrypter', () => {
   test('Should retrun MissinParam if no params is provided', async () => {
     const encrypter = new Encrypter()
+
     expect(async () => { await encrypter.compare() }).rejects.toThrow(new MissingParamError('value'))
-    expect(async () => { await encrypter.compare(null, 'any_hash ') }).rejects.toThrow(new MissingParamError('value'))
+    expect(async () => { await encrypter.compare(null, 'any_hash') }).rejects.toThrow(new MissingParamError('value'))
   })
   test('Should retrun MissinParam if no params is provided', async () => {
     const encrypter = new Encrypter()
